8c49c9d30ec2dfdb1dd6ab47464b5c3a0e4d8c1a
Neil Thawani <neilthawani@gmail.com>
2020-07-14 16:51:29 -0400

    Typo fix, stable at Hello World

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TalkRatio from './visualizations/TalkRatio';
-import { Button, Utterance } from './visualizations/Transcript';
+import Transcript from './visualizations/Transcript';
 import TurnTaking from './visualizations/TurnTaking';
 import './index.scss';
 
 ReactDOM.render(
-  <Utterance />,
+  <h1>Hello World!</h1>,
   document.getElementById('root')
 );
