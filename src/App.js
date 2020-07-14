9af34016f34adba9456432766e9ad4e00287ceaa
Neil Thawani <neilthawani@gmail.com>
2020-07-14 17:18:35 -0400

    P5 is stable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import React from 'react';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Sketch from 'react-p5';
+//
+// export default class App extends React.Component {
+//
+//     render() {
+//         return (
+//             <h1>Hello World!</h1>
+//         )
+//     }
+// }
+import React, { Component } from 'react';
+import P5Wrapper from 'react-p5-wrapper';
+// import './App.css';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {color:[Math.random()*255, Math.random()*255, Math.random()*255]};
+    this.randomColor = this.randomColor.bind(this);
+  }
 
-    render() {
-        return (
-            <h1>Hello World!</h1>
-        )
+  randomColor(){
+    this.setState({color:[Math.random()*255, Math.random()*255, Math.random()*255]}
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.randomColor}>Random Color</button>
+        <P5Wrapper sketch={sketch} color={this.state.color}></P5Wrapper>
+      </div>
+    );
+  }
+}
+
+var sketch = function(p){
+    let canvas;
+
+    p.setup = () => {
+      canvas = p.createCanvas(300, 200);
+      p.noStroke();
+    }
+
+    p.draw = () => {
+      p.background('orangered');
+      p.ellipse(150, 100, 100, 100);
+    }
+
+    p.myCustomRedrawAccordingToNewPropsHandler = (newProps) => {
+      if(canvas) //Make sure the canvas has been created
+        p.fill(newProps.color);
     }
 }
