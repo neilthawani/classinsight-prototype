5d8766b1410addf0577927e896862de6d676a545
Neil Thawani <neilthawani@gmail.com>
2020-08-10 17:12:59 -0500

    50% done with ButtonSelector

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
+## ClassInSight Prototype
+
+You can learn more about ClassInSight [here](https://www.hcii.cmu.edu/research/classinsight).
+
+## Running the App
+
+In Terminal/PowerShell, run:
+
+```
+npm install
+npm start
+```
+
+The app will be live at `localhost:3000` in your browser.
+
+## Create React App
+
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 Below you will find some information on how to perform common tasks.<br>
@@ -1516,17 +1533,17 @@ When you build the project, Create React App will place the `public` folder cont
 2. Install `serve` by running `npm install --save serve`.
 
 3. Add this line to `scripts` in `package.json`:
-    
+
     ```
     "now-start": "serve build/",
     ```
-    
+
 4. Run `now` from your project directory. You will see a **now.sh** URL in your output like this:
-    
+
     ```
     > Ready! https://your-project-dirname-tpspyhtdtk.now.sh (copied to clipboard)
     ```
-    
+
     Paste that URL into your browser when the build is complete, and you will see your deployed app.
 
 Details are available in [this article.](https://zeit.co/blog/now-static)
--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
 import React, { Component } from 'react';
-import P5Wrapper from 'react-p5-wrapper';
+import ButtonSelector from './components/ButtonSelector';
 
 export default class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            color: [Math.random()*255, Math.random()*255, Math.random()*255]
-        };
-        this.randomColor = this.randomColor.bind(this);
-    }
+    buttonSelectorOptions = ['TalkRatio', 'Transcript', 'TurnTaking'];
 
-  randomColor(){
-    this.setState({color:[Math.random()*255, Math.random()*255, Math.random()*255]}
-    )
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.randomColor}>Random Color</button>
-        <P5Wrapper sketch={sketch} color={this.state.color}></P5Wrapper>
-      </div>
-    );
-  }
-}
-
-var sketch = function(p) {
-    let canvas;
-
-    p.setup = () => {
-        canvas = p.createCanvas(300, 200);
-        p.noStroke();
-    }
-
-    p.draw = () => {
-        p.background('orangered');
-        p.ellipse(150, 100, 100, 100);
-    }
-
-    p.myCustomRedrawAccordingToNewPropsHandler = (newProps) => {
-        if(canvas) //Make sure the canvas has been created
-            p.fill(newProps.color);
+    render() {
+      return (
+        <ButtonSelector options={this.buttonSelectorOptions} />
+      )
     }
 }
