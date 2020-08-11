51f541c310d6182cccc8cbf353993f74b19f6910
Neil Thawani <neilthawani@gmail.com>
2020-08-11 15:43:19 -0500

    Button selector is working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import ButtonSelector from './components/ButtonSelector';
+import VisualizationComponents from './fixtures/visualization_components';
 
 export default class App extends Component {
-    buttonSelectorOptions = ['TalkRatio', 'Transcript', 'TurnTaking'];
+    constructor(props) {
+        super(props);
+
+        var handleClick = this.handleClick.bind(this);
+        this.state = {
+            selectedOption: Object.keys(this.components)[0]
+        };
+    }
+
+    components = VisualizationComponents;
+    buttonSelectorOptions = Object.keys(this.components);
+
+    handleClick(value) {
+        this.setState({
+            selectedOption: value
+        });
+    }
 
     render() {
-      return (
-        <ButtonSelector options={this.buttonSelectorOptions} />
-      )
+        return (
+          <div className="container">
+            <ButtonSelector
+              options={this.buttonSelectorOptions}
+              selectedOption={this.state.selectedOption}
+              onClick={this.handleClick.bind(this)} />
+
+            {React.createElement(this.components[this.state.selectedOption])}
+          </div>
+        )
     }
 }
--- src/components/ButtonSelector.js
+++ src/components/ButtonSelector.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import { capitalCase } from 'change-case';
 
 export default class ButtonSelector extends Component {
-  render() {
-    return (
-      <div className="button-selector">
-        {this.props.options.map((item, index, array) => {
-            return <button className="button-selector-item" key={index}>
-              {capitalCase(item)}
-            </button>
-        })}
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="button-selector">
+              {this.props.options.map((item, index, array) => {
+                  return <button className="button-selector-item" key={index} onClick={() => this.props.onClick(item)}>
+                    {capitalCase(item)}
+                  </button>
+              })}
+            </div>
+        )
+    }
 }
