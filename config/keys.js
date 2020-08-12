cf393f260b7640271e508dbd70a42a2495aa0c61
Neil Thawani <neilthawani@gmail.com>
2020-08-11 20:19:01 -0500

    Up to 1. Common Components: Navbar & Landing

--- config/keys.js
+++ config/keys.js
@@ -8,5 +8,6 @@
 // nodemon run server
 
 module.exports = {
-    mongoURI: "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb"
+    mongoURI: "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb",
+    secretOrKey: "secret"
 };
