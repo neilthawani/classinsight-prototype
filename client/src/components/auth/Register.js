36d128e9f9be9e92be3aad2fee7bd8354a9ffe15
Neil Thawani <neilthawani@gmail.com>
2020-08-13 16:34:07 -0500

    componentWillReceiveProps revert

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -24,7 +24,7 @@ class Register extends Component {
     }
   }
 
-  getDerivedStateFromProps(nextProps) {
+  componentWillReceiveProps(nextProps) {
     if (nextProps.errors) {
       this.setState({
         errors: nextProps.errors
