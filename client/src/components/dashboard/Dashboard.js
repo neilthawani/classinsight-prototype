17e5bb1dd43c27c90d1efea26e39bad2f07c07fe
Neil Thawani <neilthawani@gmail.com>
2020-08-13 16:02:32 -0500

    Log out via menu

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { logoutUser } from "../../actions/authActions";
 
 import ButtonSelector from '../ButtonSelector';
 import VisualizationComponents from '../../fixtures/visualization_components';
@@ -30,48 +29,17 @@ class Dashboard extends Component {
     window.localStorage.setItem("buttonSelectorSelectedOption", value);
   }
 
-  onLogoutClick = e => {
-    e.preventDefault();
-    this.props.logoutUser();
-  };
-
   render() {
     const { user } = this.props.auth;
 
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
-        <div className="row">
-          <div className="landing-copy col s12 center-align">
-            <h4>
-              <b>Hey there,</b> {user.name.split(" ")[0]}
-              <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-              </p>
-            </h4>
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
-            </button>
-          </div>
-        </div>
-
+      <div className="dashboard-container">
         <ButtonSelector
           options={this.buttonSelectorOptions}
           selectedOption={this.state.selectedOption}
           onClick={this.handleClick.bind(this)} />
 
-        <div className="visualization">
-          {React.createElement(this.components[this.state.selectedOption])}
-        </div>
+        {React.createElement(this.components[this.state.selectedOption])}
       </div>
     );
   }
@@ -87,6 +55,5 @@ const mapStateToProps = state => ({
 });
 
 export default connect(
-  mapStateToProps,
-  { logoutUser }
+  mapStateToProps
 )(Dashboard);
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,31 +1,61 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
 import ChevronDownIcon from 'mdi-react/ChevronDownIcon';
 
-
 class Navbar extends Component {
+  onLogoutClick = e => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
+
   render() {
+    const { user } = this.props.auth;
+    var isLoggedIn = Object.keys(user).length != 0 ? true : false;
+
     return (
       <div className="navbar">
-        <div class="navbar-logo">
+        <div className="navbar-logo">
           <Link to="/">Home</Link>
         </div>
 
-        <div class="navbar-dropdown">
-          <div class="navbar-dropdown-photo"></div>
-          <ChevronDownIcon color="black" size="48" />
-
-          <div class="navbar-dropdown-menu">
-            <div class="navbar-dropdown-menu-item">
-              <a class="navbar-dropdown-menu-item-link">
-                Logout
-              </a>
+        {isLoggedIn ?
+          <div className="navbar-dropdown">
+            <div className="navbar-dropdown-profile">
+              <div className="navbar-dropdown-profile-photo"></div>
+              <p className="navbar-dropdown-profile-name">
+                {user.name.split(" ")[0]}
+              </p>
             </div>
-          </div>
-        </div>
+            <ChevronDownIcon color="black" size="48" />
+
+            <div className="navbar-dropdown-menu">
+              <div className="navbar-dropdown-menu-item">
+                <a className="navbar-dropdown-menu-item-link" onClick={this.onLogoutClick}>
+                  Logout
+              </a>
+              </div>
+            </div>
+          </div> : ''
+        }
+
       </div>
     );
   }
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(Navbar);
