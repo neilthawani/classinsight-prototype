ffa623a5f8ff2b494a25e515df7b0f709a1a8b9c
Neil Thawani <neilthawani@gmail.com>
2020-09-07 11:34:57 -0500

    Refactored and componentized legend, fits in TurnTaking

---
+++ client/src/fixtures/legend_labels.js
@@ -0,0 +1,71 @@
+export default [{
+    type: "Teacher",
+    value: "Metacognitive Modeling Questions",
+    text: "Metacognitive",
+    color: "#10273d"
+}, {
+    type: "Teacher",
+    value: "Teacher Explanation + Evidence",
+    text: "Explanation + Evidence",
+    color: "#204e79"
+}, {
+    type: "Teacher",
+    value: "Teacher Open-Ended S/Q",
+    text: "Open-Ended Questions",
+    color: "#3075b5"
+}, {
+    type: "Teacher",
+    value: "Teacher Close-Ended S/Q",
+    text: "Close-Ended Questions",
+    color: "#5e9bd4"
+}, {
+    type: "Teacher",
+    value: "Assorted Teacher Talk",
+    text: "Other Talk",
+    color: "#d7e6f4"
+}, {
+    type: "Student",
+    value: "Student Explanation + Evidence",
+    text: "Explanation + Evidence",
+    color: "#562810"
+}, {
+    type: "Student",
+    value: "Student Open-Ended S/Q",
+    text: "Open-Ended Questions",
+    color: "#97471c"
+}, {
+    type: "Student",
+    value: "Student Open-Ended Response",
+    text: "Open-Ended Response",
+    color: "#d76528"
+}, {
+    type: "Student",
+    value: "Student Close-Ended S/Q",
+    text: "Close-Ended Questions",
+    color: "#e39368"
+}, {
+    type: "Student",
+    value: "Student Close-Ended Response",
+    text: "Close-Ended Response",
+    color: "#efc1a9"
+}, {
+    type: "Student",
+    value: "Assorted Student Talk",
+    text: "Other Talk",
+    color: "#fbf0e9"
+}, {
+    type: "Technique",
+    value: "Turn-Taking Facilitation",
+    text: "Turn-Taking Facilitation",
+    color: "#daacec"
+}, {
+    type: "Technique",
+    value: "Re-Voicing",
+    text: "Re-Voicing",
+    color: "#a22fd0"
+}, {
+    type: "Technique",
+    value: "Behavior Management Questions",
+    text: "Behavior Management",
+    color: "#411353"
+}];
