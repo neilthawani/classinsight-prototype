eb6ae38d4fed8ebe3cb1686e61bd31e047f0c462
Neil Thawani <neilthawani@gmail.com>
2020-09-18 10:24:53 -0500

    Cleanup

--- client/src/components/visualizations/turn-taking/Bar.js
+++ client/src/components/visualizations/turn-taking/Bar.js
@@ -37,6 +37,8 @@ export default class Bar extends Component {
 
       var { teacherStyle, studentStyle } = drawBarStyles(item);
 
+      var scriptData = Parser.filteredTranscript({activeFilters: this.props.activeFilters});
+
       return (
         <div className="turn-taking-visualization-row" onClick={this.handleClick}>
           <div className={itemTimestamp.length > 1 ? "turn-taking-bar-timestamp-range" : "turn-taking-bar-timestamp-time"}>
@@ -56,7 +58,7 @@ export default class Bar extends Component {
           {isFocusRow ?
             <div className="turn-taking-visualization-row-drilldown">
               <Script
-                data={Parser.filteredTranscript({activeFilters: this.props.activeFilters})}
+                data={scriptData}
                 focusObj={this.props.focusObj} />
             </div>
           : '' }
