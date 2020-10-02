035aebda1443a509c71dc9f177de0804a5c0fd80
Neil Thawani <neilthawani@gmail.com>
2020-10-02 11:34:24 -0500

    Initial code for Google OAuth

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,21 @@
-// To install and run MongoDB on Mac:	
-// https://docs.mongodb.com/manual/tutorial/install-mongodb-on-os-x/	
-// Note that the param `compressors=disabled&` is removed from mongoURI below due to error in Terminal:	
-// { MongoParseError: Value for `compressors` must be at least one of: `snappy`, `zlib`	
+// To install and run MongoDB on Mac:
+// https://docs.mongodb.com/manual/tutorial/install-mongodb-on-os-x/
+// Note that the param `compressors=disabled&` is removed from mongoURI below due to error in Terminal:
+// { MongoParseError: Value for `compressors` must be at least one of: `snappy`, `zlib`
 
-// To run:	
-// mongo	
+// To run:
+// mongo
 // nodemon run server
 
 module.exports = {
-  mongoURI: "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb",
-  secretOrKey: "secret"
+    mongoURI: "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb",
+    secretOrKey: "secret",
+
+    OAuth: {
+        clientID: "http://121514957489-c9r3co970umvvo2te9i9sf4h2fg1pshc.apps.googleusercontent.com/",
+        clientOrSecret: "KtRPvF6kickYwTx-sYv6aTy1",
+        callbackURL: "http://localhost:3001/auth/google/callback",
+        scope: ['profile', 'email'], // what to fetch from Google
+        passReqToCallback: true
+    }
 };
--- server.js
+++ server.js
@@ -7,11 +7,13 @@ const users = require("./routes/api/users");
 
 const app = express();
 
+const keys = require('./config/keys');
+
 // Bodyparser middleware
 app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
+    bodyParser.urlencoded({
+        extended: false
+    })
 );
 app.use(bodyParser.json());
 
@@ -19,13 +21,12 @@ app.use(bodyParser.json());
 const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
-mongoose
-  .connect(
+mongoose.connect(
     db,
     { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
+)
+.then(() => console.log("MongoDB successfully connected"))
+.catch(err => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
@@ -38,4 +39,19 @@ app.use("/api/users", users);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+// Google OAuth login/registration
+app.get('/auth/google',
+    passport.authenticate('google', { scope: keys.OAuth.scope });
+);
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+        var href = window.location.origin;
+        res.redirect(href);
+    }
+);
+
+app.listen(port, () => {
+    console.log(`Server up and running on port ${port}!`);
+});
