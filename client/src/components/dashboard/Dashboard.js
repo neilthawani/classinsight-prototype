4e00dcb5142905b08ef54884bdcb084eaa3827f4
Neil Thawani <neilthawani@gmail.com>
2020-10-12 11:10:53 -0500

    Fixed localStorage of buttonselector options

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -11,78 +11,62 @@ import { connect } from "react-redux";
 // import TurnTaking from '../visualizations/turn-taking/TurnTaking';
 
 class Dashboard extends Component {
-    // constructor(props) {
-    //     super(props);
-    //
-    //     console.log("here", props);
-        // this.state = {
-        //     selectedOption: localStorage.getItem("buttonSelectorSelectedOption")
-        // };
-    // }
+    constructor(props) {
+        super(props);
 
-    // componentDidMount() {
-    //     // If logged in and user navigates to Register page, should redirect them to dashboard
-    //     this.props.history.push(`/dashboard/${this.state.selectedOption}`);
-    // }
-//         this.state = {
-//             buttonSelectorSelectedOption: localStorage.getItem("buttonSelectorSelectedOption"),
-//             transcriptLocationHash: localStorage.getItem("transcriptLocationHash") || ""
-//         };
-//     }
-//     componentWillMount() {
-//         // update ButtonSelector selected option on drilldown
-//         this.unlisten = this.props.history.listen((location, action) => {
-//             var buttonSelectorSelectedOption = location.pathname.slice(location.pathname.lastIndexOf("/") + 1);
-//             var transcriptLocationHash = location.hash || "";
+        console.log("here", props);
+        this.state = {
+            buttonSelectorSelectedOption: localStorage.getItem("buttonSelectorSelectedOption")
+        };
+    }
 
-//             localStorage.setItem("buttonSelectorSelectedOption", buttonSelectorSelectedOption);
-//             localStorage.setItem("transcriptLocationHash", transcriptLocationHash);
+    componentDidMount() {
+        console.log("this.state.buttonSelectorSelectedOption", this.state.buttonSelectorSelectedOption);
+        // If logged in and user navigates to Register page, should redirect them to dashboard
 
-//             this.setState({
-//                 buttonSelectorSelectedOption: buttonSelectorSelectedOption,
-//                 transcriptLocationHash: transcriptLocationHash
-//             });
-//         });
-//     }
 
-//     componentWillUnmount() {
-//         this.unlisten();
-//     }
+        // transcriptLocationHash = this.state.transcriptLocationHash || "";
+        //
+        // this.props.history.push(`/dashboard/${buttonSelectorSelectedOption}${transcriptLocationHash}`);
 
-//     componentDidMount() {
-//         // If logged in and user navigates to Register page, should redirect them to dashboard
-//         var buttonSelectorSelectedOption = this.state.buttonSelectorSelectedOption,
-//             transcriptLocationHash = this.state.transcriptLocationHash || "";
+        if (this.state.buttonSelectorSelectedOption === "dashboard") {
+            this.setState({
+                buttonSelectorSelectedOption: ""
+            });
+        }
 
-//         this.props.history.push(`/dashboard/${buttonSelectorSelectedOption}${transcriptLocationHash}`);
-//     }
+        this.props.history.push(`${this.state.buttonSelectorSelectedOption}`);
 
-//     handleClick(value) {
-//         localStorage.setItem("buttonSelectorSelectedOption", value);
-//         this.setState({
-//             buttonSelectorSelectedOption: value
-//         });
-//     }
+        this.setState = ({
+            buttonSelectorSelectedOption: localStorage.getItem("buttonSelectorSelectedOption"),
+            transcriptLocationHash: localStorage.getItem("transcriptLocationHash") || ""
+        });
+    }
+    componentWillMount() {
+        var that = this;
+
+        // update ButtonSelector selected option on drilldown
+        this.unlisten = this.props.history.listen((location, action) => {
+            var buttonSelectorSelectedOption = location.pathname.slice(location.pathname.lastIndexOf("/") + 1);
+            var transcriptLocationHash = location.hash || "";
+
+            localStorage.setItem("buttonSelectorSelectedOption", buttonSelectorSelectedOption);
+            localStorage.setItem("transcriptLocationHash", transcriptLocationHash);
+
+            that.setState({
+                buttonSelectorSelectedOption: buttonSelectorSelectedOption,
+                transcriptLocationHash: transcriptLocationHash
+            });
+        });
+    }
+
+    componentWillUnmount() {
+        this.unlisten();
+    }
 
     render() {
-        // A <Switch> looks through all its children <Route> elements and
-        //   renders the first one whose path matches the current URL.
-        //   Use a <Switch> any time you have multiple routes,
-        //   but you want only one of them to render at a time.
-
-        // <PrivateRoute exact path="/dashboard" component={Dashboard} />
         return (
           <div className="dashboard-container">
-            {/* coarse, medium, and fine-grained visualizations */}
-            {/*<ButtonSelector
-              selectedOption={this.state.buttonSelectorSelectedOption}
-              handleClick={this.handleClick.bind(this)} />
-
-            <Switch>
-              <PrivateRoute exact path="/dashboard/talk-ratio" component={TalkRatio} />
-              <PrivateRoute exact path="/dashboard/turn-taking" component={TurnTaking} />
-              <PrivateRoute exact path="/dashboard/transcript" component={Transcript} />
-            </Switch>*/}
           </div>
         );
     }
