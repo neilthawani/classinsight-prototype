0b6d15ef6f4fdcab391f85c354f4f48cfe84a6dd
Neil Thawani <neilthawani@gmail.com>
2020-11-17 13:32:17 -0600

    Update types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -7,6 +7,4 @@ export const SET_CURRENT_USER = "SET_CURRENT_USER";
 // adminActions
 export const LIST_USERS = "LIST_USERS";
 export const CREATE_USER = "CREATE_USER";
-export const EDIT_USER = "EDIT_USER";
 export const DELETE_USER = "DELETE_USER";
-export const ADD_USER_TO_USERS_LIST = "ADD_USER_TO_USERS_LIST";
