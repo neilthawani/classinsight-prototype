a380628cb3defa13854744a7096106524e6d953b
Neil Thawani <neilthawani@gmail.com>
2020-12-30 11:08:33 -0600

    Retrieve user if active

--- routes/api/users.js
+++ routes/api/users.js
@@ -214,11 +214,13 @@ router.post('/delete', (req, res) => {
 router.get('/list', function(req, res) {
     User.find({}, function(error, users) {
         var parsedUsers = users.map((user) => {
-            return {
-                _id: user._id,
-                userType: user.userType,
-                name: user.name,
-                email: user.email
+            if (user.isActive) {
+                return {
+                    _id: user._id,
+                    userType: user.userType,
+                    name: user.name,
+                    email: user.email
+                }
             }
         })
         res.send(parsedUsers);
