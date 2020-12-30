99992de04f9ec79c42483423d04a5616f81c722e
Neil Thawani <neilthawani@gmail.com>
2020-12-29 22:09:43 -0600

    Reset dataset fetch on logo click

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { listDatasets } from "../../actions/datasetActions";
 
 class Landing extends Component {
   componentDidMount() {
+    // console.log("Landing componentDidMount")
     // If logged in and user navigates to Register page, should redirect them to dashboard
     if (this.props.auth.isAuthenticated) {
+      this.props.listDatasets(this.props.auth.user.id);
+      // console.log("redirect");
       this.props.history.push("/dashboard");
     }
   }
@@ -27,14 +31,17 @@ class Landing extends Component {
 
 // export default Landing;
 Landing.propTypes = {
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  datasets: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
   auth: state.auth,
-  errors: state.errors
+  errors: state.errors,
+  datasets: state.datasets
 });
 
-export default connect(
-  mapStateToProps
-)(withRouter(Landing));
+export default withRouter(connect(
+  mapStateToProps,
+  { listDatasets }
+)(Landing));
