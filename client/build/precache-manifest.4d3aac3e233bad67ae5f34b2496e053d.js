e646371ae6a86f68784194699a13ae141905e8f3
Ketaki Rao <ketaki_8@hotmail.com>
2021-01-14 21:29:49 -0500

    Port

---
+++ client/build/precache-manifest.4d3aac3e233bad67ae5f34b2496e053d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a37e82782c363dcde69435c862e206cc",
+    "url": "/static/media/classinsight-logo.a37e8278.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "267da2c1ebfb0e0a772e",
+    "url": "/static/js/main.267da2c1.chunk.js"
+  },
+  {
+    "revision": "544f9b57a05d09e27004",
+    "url": "/static/js/1.544f9b57.chunk.js"
+  },
+  {
+    "revision": "267da2c1ebfb0e0a772e",
+    "url": "/static/css/main.485478d9.chunk.css"
+  },
+  {
+    "revision": "059b33a2adfc2eff8ab6c206d00dc37b",
+    "url": "/index.html"
+  }
+];
