19968f4602bdb24a4da1ac1ffacef5b79547eb67
Neil Thawani <neilthawani@gmail.com>
2021-01-24 18:00:39 -0600

    Trend line is stable

--- client/src/components/visualizations/dashboard/TrendChart.js
+++ client/src/components/visualizations/dashboard/TrendChart.js
@@ -1,49 +1,43 @@
 import React, { Component } from "react";
 import * as d3 from 'd3';
 import TrendLine from './TrendLine';
-import { removePropertyFromObject } from '../../../utils/removePropertyFromObject';
-import flattenArray from '../../../utils/flattenArray';
-import toCamelCase from '../../../utils/toCamelCase';
+// import { removePropertyFromObject } from '../../../utils/removePropertyFromObject';
+// import flattenArray from '../../../utils/flattenArray';
+// import toCamelCase from '../../../utils/toCamelCase';
 import TrendChartAxis from './TrendChartAxis';
 
 class TrendChart extends Component {
     render() {
         // console.log("this.props.axes", this.props.axes);
         var xScale = this.props.xScale,
-            yScale = this.props.yScale;
+            yScale = this.props.yScale,
+            trendLine = d3.line().x((d) => xScale(d.date)).y((d) => yScale(d.score));
 
         return (
             <svg className="trend-chart-svg" width={this.props.svgWidth} height={this.props.svgHeight}>
-              <TrendChartAxis
-                className="x axis"
-                transform={this.props.xAxisTransform}
-                axis={this.props.axes.xAxis} />
-
-              <TrendChartAxis
-                className="y axis"
-                transform={this.props.yAxisTransform}
-                axis={this.props.axes.yAxis} />
-
                 {this.props.trendLineData.map((trendLineDatum, index, array) => {
-                    // console.log("trendLineDatum", trendLineDatum);
-                    var trendLine = d3.line().x((d) => xScale(d.date)).y((d) => yScale(d.score));
+                    var color = trendLineDatum[0].barColor;
 
                     return (
-                        <g key={`group-${index}`} className="trend-chart-path-group">
-                          <path
-                            key={`path-${index}`}
-                            className="trend-chart-path"
-                            d={trendLine(trendLineDatum)}
-                            stroke={trendLineDatum[0].barColor} />
-
-                          {trendLineDatum.map((point, jindex) => {
-                            return (
-                              <circle key={`circle-${index}-${jindex}`} r="4" fill={point.barColor} cx={xScale(point.date)} cy={yScale(point.score)} />
-                            )
-                          })}
-                        </g>
-                    )
+                      <TrendLine
+                        key={index}
+                        xScale={xScale}
+                        yScale={yScale}
+                        trendLine={trendLine}
+                        trendLineDatum={trendLineDatum}
+                        color={color} />
+                    );
                 })}
+
+                <TrendChartAxis
+                  className="x axis"
+                  transform={this.props.xAxisTransform}
+                  axis={this.props.axes.xAxis} />
+
+                <TrendChartAxis
+                  className="y axis"
+                  transform={this.props.yAxisTransform}
+                  axis={this.props.axes.yAxis} />
             </svg>
         );
     }
--- client/src/components/visualizations/dashboard/TrendLine.js
+++ client/src/components/visualizations/dashboard/TrendLine.js
@@ -2,21 +2,24 @@ import React, { Component } from "react";
 
 export default class TrendLine extends Component {
     render() {
+        var xScale = this.props.xScale,
+            yScale = this.props.yScale,
+            trendLine = this.props.trendLine,
+            trendLineDatum = this.props.trendLineDatum,
+            color = this.props.color;
+
         return (
-          <g>
-           <path
-              className="trend-chart-path goal"
-              d={this.props.trendLine.trendLine(this.props.trendChartData)}
-           />
-           {this.props.trendChartData.map((point, index) => {
-             return (
-               <circle
-                key={`circle-${index}`}
-                r="3"
-                cx={this.props.xScale(point.date)}
-                cy={this.props.yScale(point.score)} />
-             )
-           })}
+          <g className="trend-chart-path-group">
+            <path
+              className="trend-chart-path"
+              d={trendLine(trendLineDatum)}
+              stroke={color} />
+
+            {trendLineDatum.map((point, index) => {
+              return (
+                <circle key={`circle-${index}`} r="4" fill={point.barColor} cx={xScale(point.date)} cy={yScale(point.score)} />
+              )
+            })}
           </g>
         );
     }
