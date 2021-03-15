2fea80e94b81f879123a29a4796809079405b427
Neil Thawani <neilthawani@gmail.com>
2021-03-15 12:10:46 -0500

    Change port to 8802

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
         open: true,
         historyApiFallback: true,
         proxy: {
-            '/api': 'http://localhost:8080'
+            '/api': 'http://localhost:8802'
         },
         disableHostCheck: true
     },
