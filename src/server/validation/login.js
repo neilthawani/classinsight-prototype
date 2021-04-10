5f67bf74d6fea20a7ce2b3cabf237b93184c3908
Neil Thawani <neilthawani@gmail.com>
2021-04-10 17:53:08 -0500

    Remove email validation

--- src/server/validation/login.js
+++ src/server/validation/login.js
@@ -5,15 +5,16 @@ module.exports = function validateLoginInput(data) {
   let errors = {};
 
   // Convert empty fields to an empty string so we can use validator functions
-  data.email = !isEmpty(data.email) ? data.email : "";
+  data.usernameOrEmail = !isEmpty(data.usernameOrEmail) ? data.usernameOrEmail : "";
   data.password = !isEmpty(data.password) ? data.password : "";
 
   // Email checks
-  if (Validator.isEmpty(data.email)) {
-    errors.email = "Email field is required";
-  } else if (!Validator.isEmail(data.email)) {
-    errors.email = "Email is invalid";
+  if (Validator.isEmpty(data.usernameOrEmail)) {
+    errors.email = "Username or email is required";
   }
+  // else if (!Validator.isEmail(data.usernameOrEmail)) {
+  //   errors.email = "Email is invalid";
+  // }
   // Password checks
   if (Validator.isEmpty(data.password)) {
     errors.password = "Password field is required";
